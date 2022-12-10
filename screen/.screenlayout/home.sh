52ccba669deb9f39059b576a4d13b1d54b7758e2
Manuel González <manuel@manuel.is>
2022-12-10 17:06:05 +0200

    Add screen layout configs

---
+++ screen/.screenlayout/home.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --mode 1920x1080 --pos 0x1080 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --primary --mode 3840x2160 --pos 1920x0 --rotate normal --output VIRTUAL1 --off
