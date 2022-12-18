f6c5a073c81e77f2ffefe19e9e3262df56132b02
Manuel González <manuel@manuel.is>
2022-12-18 15:20:27 +0200

    Add fish configs

---
+++ config/.config/fish/conf.d/config.fish
@@ -0,0 +1,23 @@
+set -gx GOPATH ~/workbench/go
+set -gx KUBECONFIG ~/.kube/config
+set -gx DOCKER_BUILDKIT 1
+set -gx NVM_DIR /home/manuel/.nvm
+set -gx LD_LIBRARY_PATH /usr/local/lib
+set -gx USE_GKE_GCLOUD_AUTH_PLUGIN true
+contains /usr/local/go/bin $fish_user_paths; or set -Ua fish_user_paths /usr/local/go/bin
+contains ~/workbench/go/bin $fish_user_paths; or set -Ua fish_user_paths ~/workbench/go/bin
+contains ~/.krew/bin $fish_user_paths; or set -Ua fish_user_paths ~/.krew/bin
+# contains ~/.local/bin $fish_user_paths; or set -Ua fish_user_paths ~/.local/bin
+
+# No greeting
+set fish_greeting
+
+# cd -
+abbr -a -- - 'cd -'
+
+# Theme related
+set -g theme_display_date no
+set -g theme_display_cmd_duration no
+
+set -g fish_term24bit 1
+rvm default
