9d39ebd7f73f27ee77af46b88d681914bd43f743
Manuel González <manuel@manuel.is>
2023-10-30 09:49:49 +0200

    Remove Kubernetes wrappers and tooling

--- config/.config/fish/conf.d/config.fish
+++ config/.config/fish/conf.d/config.fish
@@ -1,13 +1,10 @@
 set -gx SHELL /usr/bin/fish
 set -gx EDITOR ~/.local/bin/et
 set -gx GOPATH ~/workbench/go
-set -gx KUBECONFIG ~/.kube/config
 set -gx DOCKER_BUILDKIT 1
 set -gx NVM_DIR /home/manuel/.nvm
 contains /usr/local/go/bin $fish_user_paths; or set -Ua fish_user_paths /usr/local/go/bin
 contains ~/workbench/go/bin $fish_user_paths; or set -Ua fish_user_paths ~/workbench/go/bin
-contains ~/.krew/bin $fish_user_paths; or set -Ua fish_user_paths ~/.krew/bin
-# contains ~/.local/bin $fish_user_paths; or set -Ua fish_user_paths ~/.local/bin
 
 # No greeting
 set fish_greeting
