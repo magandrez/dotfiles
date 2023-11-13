ae26de6dae2381531ba4db9be88557b9c1bc7867
Manuel González <manuel@manuel.is>
2023-11-13 10:51:30 +0200

    Add gcloud CLI completion to PATH

--- config/.config/fish/config.fish
+++ config/.config/fish/config.fish
@@ -1,3 +1,6 @@
 # Load RVM
 # rvm default
 # load_nvm > /dev/stderr
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/usr/local/google-cloud-sdk/path.fish.inc' ]; . '/usr/local/google-cloud-sdk/path.fish.inc'; end
