54362d356e399db178b350fb4ea98c8998f430e9
Manuel González <manuel@manuel.is>
2024-01-03 17:03:15 +0200

    Rename all outputs as per xrandr (updated) output

--- screen/.screenlayout/home.sh
+++ screen/.screenlayout/home.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --primary --mode 3840x2160 --pos 1920x0 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off
+xrandr --output eDP1 --mode 1920x1200 --pos 0x960 --rotate normal --output DP1 --primary --mode 3840x2160 --pos 1920x0 --rotate normal --output DP2 --off --output DP3 --off --output DP4 --off --output HDMI1 --off --output VIRTUAL1 --off
