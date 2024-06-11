7c4e0624ed35a2780ae3bc959309e47468fee4a0
Manuel González <manuel@manuel.is>
2024-06-11 15:07:46 +0300

    Remove unnecessary gcloud sourcing

--- config/.config/fish/config.fish
+++ config/.config/fish/config.fish
@@ -2,6 +2,4 @@
 # rvm default
 # load_nvm > /dev/stderr
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/usr/local/bin/gcloud/path.fish.inc' ]; . '/usr/local/bin/gcloud/path.fish.inc'; end
 direnv hook fish | source
