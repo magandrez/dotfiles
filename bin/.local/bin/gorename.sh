33eb07047cc39c5cbabb9adcff63968ab264672d
Manuel González <manuel@manuel.is>
2024-08-16 09:32:57 +0300

    Provision script to rename Golang repos

---
+++ bin/.local/bin/gorename.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+go mod edit -module "${2}"
+find . -type f -name '*.go' -exec sed -i -e "s;\"${1}/;\"${2}/;g" {} \;
+find . -type f -name '*.go' -exec sed -i -e "s;\"${1}\";\"${2}\";g" {} \;
