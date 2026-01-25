3affce18bcef9188d05126488b6742ab34cfebd2
Manuel González <manuel@manuel.is>
2026-01-25 12:38:44 +0100

    Revert "Move grog alias into a fish function to enable passing args"
    This reverts commit 3fcfa25b4b91834fa045fcdcfb4d6b6f3b52e321.

--- config/.config/fish/functions/git.fish
+++
@@ -1,3 +0,0 @@
-function grog
-    git log --graph --abbrev-commit --decorate --all --format=format:"%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(dim white) - %an%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n %C(white)%s%C(reset)" $argv
-end
