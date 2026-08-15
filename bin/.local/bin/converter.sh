e30d63173e3df63e0e706889ed4d2be1edcd731b
Manuel González <manuel@manuel.is>
2026-08-15 14:26:49 +0300

    Documented converter script

--- bin/.local/bin/converter.sh
+++ bin/.local/bin/converter.sh
@@ -1,4 +1,14 @@
-#!/bin/sh
+#!/usr/bin/env bash
+#
+# Author: Manuel González <manuel@manuel.is> -  22.10.2024
+#
+# Requirements
+#
+# - ffmpeg
+#
+# Description
+#
+# FLAC mass converter from m4a into FLAC utilizing ffmpeg
 
 for file in *.m4a; do
     echo "Converting $file to flac";
